c161f2d4ebe964c425cee3454b1e800e7e475242
Johannes Quack <quackjohannes@gmail.com>
2020-03-04 16:36:11 +0100

    #23 implement theme switching

--- frontend/src/app/services/theme.service.ts
+++ frontend/src/app/services/theme.service.ts
@@ -13,29 +13,29 @@ export class ThemeService {
 
   private renderer: Renderer2;
   private activeTheme: AvailableThemes;
-  private _darkThemeActive = new ReplaySubject<boolean>(1);
-  public darkThemeActive$ = this._darkThemeActive.asObservable();
+  private darkThemeActive = new ReplaySubject<boolean>(1);
+  public darkThemeActive$ = this.darkThemeActive.asObservable();
 
   constructor(private rendererFactory: RendererFactory2) {
     this.renderer = this.rendererFactory.createRenderer(null, null);
     this.activeTheme = AvailableThemes.dark;
-    this._darkThemeActive.next(true);
+    this.darkThemeActive.next(true);
   }
 
   public enableTheme(theme: AvailableThemes): void {
     if (theme === this.activeTheme) { return; }
-    switch(theme) {
+    switch (theme) {
       case AvailableThemes.light:
         this.renderer.removeClass(document.documentElement, 'dark-theme');
         this.renderer.addClass(document.documentElement, 'light-theme');
         this.activeTheme = AvailableThemes.light;
-        this._darkThemeActive.next(false);
+        this.darkThemeActive.next(false);
         break;
       case AvailableThemes.dark:
         this.renderer.removeClass(document.documentElement, 'light-theme');
         this.renderer.addClass(document.documentElement, 'dark-theme');
         this.activeTheme = AvailableThemes.dark;
-        this._darkThemeActive.next(true);
+        this.darkThemeActive.next(true);
         break;
     }
   }
