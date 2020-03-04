1c101a38e1b3563940b3f20116dfcad97447d91f
Peter Topfstedt <peter.topfstedt@gmail.com>
2020-03-04 15:44:32 +0100

    #23 add theming and theme switching functionality
    colors still wip

---
+++ frontend/src/app/services/theme.service.ts
@@ -0,0 +1,42 @@
+import { Injectable, Renderer2, RendererFactory2 } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
+
+export enum AvailableThemes {
+  dark,
+  light
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThemeService {
+
+  private renderer: Renderer2;
+  private activeTheme: AvailableThemes;
+  private _darkThemeActive = new ReplaySubject<boolean>(1);
+  public darkThemeActive$ = this._darkThemeActive.asObservable();
+
+  constructor(private rendererFactory: RendererFactory2) {
+    this.renderer = this.rendererFactory.createRenderer(null, null);
+    this.activeTheme = AvailableThemes.dark;
+    this._darkThemeActive.next(true);
+  }
+
+  public enableTheme(theme: AvailableThemes): void {
+    if (theme === this.activeTheme) { return; }
+    switch(theme) {
+      case AvailableThemes.light:
+        this.renderer.removeClass(document.documentElement, 'dark-theme');
+        this.renderer.addClass(document.documentElement, 'light-theme');
+        this.activeTheme = AvailableThemes.light;
+        this._darkThemeActive.next(false);
+        break;
+      case AvailableThemes.dark:
+        this.renderer.removeClass(document.documentElement, 'light-theme');
+        this.renderer.addClass(document.documentElement, 'dark-theme');
+        this.activeTheme = AvailableThemes.dark;
+        this._darkThemeActive.next(true);
+        break;
+    }
+  }
+}
