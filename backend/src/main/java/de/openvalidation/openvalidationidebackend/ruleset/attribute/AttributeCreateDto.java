ac46604fab8453c6f15f725c5422823be79c6042
Stephan Hagge <Stephan.Hagge@aalberts-mt.com>
2020-03-09 12:18:49 +0100

    #19 Set required fields for create attribute

--- backend/src/main/java/de/openvalidation/openvalidationidebackend/ruleset/attribute/AttributeCreateDto.java
+++ backend/src/main/java/de/openvalidation/openvalidationidebackend/ruleset/attribute/AttributeCreateDto.java
@@ -3,12 +3,15 @@ package de.openvalidation.openvalidationidebackend.ruleset.attribute;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
 import java.util.Set;
 
 @Getter
 @Setter
 public class AttributeCreateDto {
+  @NotNull
   private String name;
+  @NotNull
   private AttributeType attributeType;
   private String value;
   private Set<AttributeCreateDto> children;
