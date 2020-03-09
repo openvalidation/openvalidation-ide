6a2403f6599bde31ddbbc9f72a6c1c7045ecab06
Peter Topfstedt <peter.topfstedt@gmail.com>
2020-03-09 16:59:40 +0100

    wip overview page layout

--- frontend/src/app/rulesets-overview/rulesets-overview.component.ts
+++ frontend/src/app/rulesets-overview/rulesets-overview.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import * as faker from 'faker';
+
+export interface RulesetDTO {
+  id: string,
+  name: string;
+  description: string;
+  createdAt: Date;
+  createdBy: string;
+}
 
 @Component({
   selector: 'ovide-rulesets-overview',
@@ -7,9 +16,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RulesetsOverviewComponent implements OnInit {
 
+  rulesets: RulesetDTO[];
+
   constructor() { }
 
   ngOnInit(): void {
+
+    const fakeRuleset= function(): RulesetDTO {
+      return {
+        id: faker.random.uuid(),
+        name: faker.company.catchPhrase(),
+        description: faker.lorem.paragraph(),
+        createdAt: faker.date.past(),
+        createdBy: faker.name.findName()
+      }
+    };
+
+    this.rulesets = Array.from({length: 20}, fakeRuleset)
   }
 
 }
