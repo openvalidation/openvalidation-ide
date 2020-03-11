99d49767ed5a4d7c5913ecd94018be55cbb1255a
aletutto <t.alessandro@web.de>
2020-03-11 11:49:26 +0100

    #48 Added CORS header to allow cross origins

--- backend/src/main/java/de/openvalidation/openvalidationidebackend/ruleset/RulesetController.java
+++ backend/src/main/java/de/openvalidation/openvalidationidebackend/ruleset/RulesetController.java
@@ -15,6 +15,7 @@ import javax.validation.Valid;
 import java.util.List;
 import java.util.Set;
 
+@CrossOrigin
 @RestController
 public class RulesetController {
   private RulesetService rulesetService;
