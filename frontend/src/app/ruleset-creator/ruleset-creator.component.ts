8794858584827a958d227b8c612e35541afc14a9
xLacrima <44511324+xLacrima@users.noreply.github.com>
2020-03-11 15:28:24 +0100

    load rulesets from backend & save new rulesets in backend

--- frontend/src/app/ruleset-creator/ruleset-creator.component.ts
+++ frontend/src/app/ruleset-creator/ruleset-creator.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { RulesetsService, RulesetCreateDto } from '@ovide/backend';
+
+import * as faker from 'faker';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'ovide-ruleset-creator',
@@ -12,17 +17,43 @@ export class RulesetCreatorComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
+    private rulesetsService: RulesetsService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
       name: ['', [Validators.required]],
-      description: ['']
+      description: [''],
+      creator: ['', [Validators.required]]
     });
   }
 
   submit() {
+    this.form.disable();
 
+    const newRuleset: RulesetCreateDto = {
+      name: this.form.value.name,
+      description: this.form.value.description,
+      createdBy: this.form.value.creator
+    }
+
+    this.rulesetsService.createRuleset(newRuleset).subscribe(
+      success => { 
+        this.router.navigate(['/rulesets' ,success.rulesetId, 'edit']);
+      },
+      error => {
+        console.error(error);
+      }
+    );
+  }
+
+  createRandom() {
+    this.rulesetsService.createRuleset({
+      name: faker.company.catchPhrase(),
+      createdBy: faker.name.findName(),
+      description: faker.lorem.paragraph()
+    }).subscribe();
   }
 
 }
--- frontend/src/app/rulesets-overview/rulesets-overview.component.ts
+++ frontend/src/app/rulesets-overview/rulesets-overview.component.ts
@@ -1,13 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import * as faker from 'faker';
-
-export interface RulesetDTO {
-  id: string,
-  name: string;
-  description: string;
-  createdAt: Date;
-  createdBy: string;
-}
+import { RulesetDto, RulesetsService } from '@ovide/backend';
 
 @Component({
   selector: 'ovide-rulesets-overview',
@@ -16,23 +8,17 @@ export interface RulesetDTO {
 })
 export class RulesetsOverviewComponent implements OnInit {
 
-  rulesets: RulesetDTO[];
+  rulesets: RulesetDto[];
 
-  constructor() { }
+  constructor(
+    private rulesetService: RulesetsService
+  ) { }
 
   ngOnInit(): void {
-
-    const fakeRuleset= function(): RulesetDTO {
-      return {
-        id: faker.random.uuid(),
-        name: faker.company.catchPhrase(),
-        description: faker.lorem.paragraph(),
-        createdAt: faker.date.past(),
-        createdBy: faker.name.findName()
-      }
-    };
-
-    this.rulesets = Array.from({length: 20}, fakeRuleset)
+    
+    this.rulesetService.getAllRulesets().subscribe(
+      rulesets => this.rulesets = rulesets
+    );
   }
 
 }
