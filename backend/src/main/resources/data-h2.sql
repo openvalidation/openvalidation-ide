78347f92721ccd39e8ccfec7c8c0f2aeb03ea677
aletutto <t.alessandro@web.de>
2020-03-16 16:27:18 +0100

    #37 Fixed h2 initialization

--- backend/src/main/resources/data-h2.sql
+++ backend/src/main/resources/data-h2.sql
@@ -1,24 +1,24 @@
 INSERT INTO schema (schema_id)
-VALUES ('9d203228-678a-11ea-bc55-0242ac130003');
+VALUES (CAST('9d203228-678a-11ea-bc55-0242ac130003' as UUID));
 
 INSERT INTO attribute (attribute_id, attribute_type, name, value)
-VALUES ('3e98ccbe-678b-11ea-bc55-0242ac130003', 2, 'name', 'Satoshi');
+VALUES (CAST('3e98ccbe-678b-11ea-bc55-0242ac130003' as UUID), 2, 'name', 'Satoshi');
 INSERT INTO attribute (attribute_id, attribute_type, name, value)
-VALUES ('97aa6d3d-8274-4093-b24a-35eeebc588e8', 1, 'age', '25');
+VALUES (CAST('97aa6d3d-8274-4093-b24a-35eeebc588e8' as UUID), 1, 'age', '25');
 INSERT INTO attribute (attribute_id, attribute_type, name, value)
-VALUES ('30f8734b-cd81-4725-a3d6-8d9ef2755795', 2, 'location', 'Dortmund');
+VALUES (CAST('30f8734b-cd81-4725-a3d6-8d9ef2755795' as UUID), 2, 'location', 'Dortmund');
 
 INSERT INTO schema_attributes (schema_schema_id, attributes_attribute_id)
-VALUES ('9d203228-678a-11ea-bc55-0242ac130003',
-        '3e98ccbe-678b-11ea-bc55-0242ac130003');
+VALUES (CAST('9d203228-678a-11ea-bc55-0242ac130003' as UUID),
+        CAST('3e98ccbe-678b-11ea-bc55-0242ac130003' as UUID));
 INSERT INTO schema_attributes (schema_schema_id, attributes_attribute_id)
-VALUES ('9d203228-678a-11ea-bc55-0242ac130003',
-        '97aa6d3d-8274-4093-b24a-35eeebc588e8');
+VALUES (CAST('9d203228-678a-11ea-bc55-0242ac130003' as UUID),
+        CAST('97aa6d3d-8274-4093-b24a-35eeebc588e8' as UUID));
 INSERT INTO schema_attributes (schema_schema_id, attributes_attribute_id)
-VALUES ('9d203228-678a-11ea-bc55-0242ac130003',
-        '30f8734b-cd81-4725-a3d6-8d9ef2755795');
+VALUES (CAST('9d203228-678a-11ea-bc55-0242ac130003' as UUID),
+        CAST('30f8734b-cd81-4725-a3d6-8d9ef2755795' as UUID));
 
 INSERT INTO ruleset (ruleset_Id, created_at, created_by, description, name, rules, schema_schema_id)
-VALUES ('d0d95814-678b-11ea-bc55-0242ac130003', '2020-03-11 20:20:34.884', 'Validaria', 'Validates the name',
+VALUES (CAST('d0d95814-678b-11ea-bc55-0242ac130003' as UUID), '2020-03-11 20:20:34.884', 'Validaria', 'Validates the name',
         'Name validation', 'your name HAS to be Validaria',
-        '9d203228-678a-11ea-bc55-0242ac130003');
+        CAST('9d203228-678a-11ea-bc55-0242ac130003' as UUID));
