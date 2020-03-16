449780c63b5cdd7a1e77b0bbbc3b5bda7f953756
Stephan Hagge <Stephan.Hagge@aalberts-mt.com>
2020-03-16 12:23:24 +0100

    #53 Handle child attributes

--- backend/src/main/java/de/openvalidation/openvalidationidebackend/entities/attribute/Attribute.java
+++ backend/src/main/java/de/openvalidation/openvalidationidebackend/entities/attribute/Attribute.java
@@ -3,10 +3,7 @@ package de.openvalidation.openvalidationidebackend.entities.attribute;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import java.util.HashSet;
+import javax.persistence.*;
 import java.util.Set;
 import java.util.UUID;
 
@@ -19,19 +16,40 @@ public class Attribute {
   private String name;
   private AttributeType attributeType;
   private String value;
-  @OneToMany
+  @OneToMany(cascade = CascadeType.ALL,
+      fetch = FetchType.EAGER)
   private Set<Attribute> children;
 
   public Attribute() {
     this.attributeId = UUID.randomUUID();
-    this.children = new HashSet<>();
+  }
+
+  public void setAttributeType(AttributeType attributeType) {
+    this.attributeType = attributeType;
+    if (attributeType != AttributeType.OBJECT && attributeType != AttributeType.LIST) {
+      this.children = null;
+    } else {
+      this.value = null;
+    }
+  }
+
+  public void setValue(String value) {
+    if (attributeType != AttributeType.OBJECT && attributeType != AttributeType.LIST) {
+      this.value = value;
+    }
+  }
+
+  public void setChildren(Set<Attribute> children) {
+    if (this.attributeType == AttributeType.OBJECT || this.attributeType == AttributeType.LIST) {
+      this.children = children;
+    }
   }
 
   public Attribute updateByAttribute(Attribute attribute) {
     this.name = attribute.name != null ? attribute.name : this.name;
-    this.attributeType = attribute.attributeType != null ? attribute.attributeType : this.attributeType;
+    this.setAttributeType(attribute.attributeType != null ? attribute.attributeType : this.attributeType);
     this.value = attribute.value != null ? attribute.value : this.value;
-    this.children = attribute.children != null && !attribute.children.isEmpty() ? attribute.children : this.children;
+    this.setChildren(attribute.children != null && !attribute.children.isEmpty() ? attribute.children : this.children);
     return this;
   }
 }
--- backend/src/main/java/de/openvalidation/openvalidationidebackend/entities/attribute/AttributeUpdateDto.java
+++ backend/src/main/java/de/openvalidation/openvalidationidebackend/entities/attribute/AttributeUpdateDto.java
@@ -3,12 +3,14 @@ package de.openvalidation.openvalidationidebackend.entities.attribute;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
 import java.util.Set;
 
 @Getter
 @Setter
 public class AttributeUpdateDto {
   private String name;
+  @NotNull
   private AttributeType attributeType;
   private String value;
   private Set<AttributeUpdateDto> children;
