423a40ee8e84e6f46aa1c7379ada22c212170e1d
Johannes Quack <quackjohannes@gmail.com>
2020-03-18 17:50:18 +0100

    #44 continue integration of monaco editor and openvalidation language server

--- frontend/src/app/ruleset-editor/ruleset-editor.component.ts
+++ frontend/src/app/ruleset-editor/ruleset-editor.component.ts
@@ -4,6 +4,9 @@ import { map, switchMap } from 'rxjs/operators';
 import { RulesetsService, RulesetDto } from '@ovide/backend';
 import { Observable } from 'rxjs';
 import { ThemeService } from '@ovide/services/theme.service';
+import { MonacoLanguageClient, CloseAction, ErrorAction, MonacoServices, createConnection } from 'monaco-languageclient';
+import { listen, MessageConnection } from 'vscode-ws-jsonrpc';
+const ReconnectingWebSocket = require('reconnecting-websocket');
 
 @Component({
   selector: 'ovide-ruleset-editor',
@@ -11,9 +14,10 @@ import { ThemeService } from '@ovide/services/theme.service';
   styleUrls: ['./ruleset-editor.component.scss']
 })
 export class RulesetEditorComponent implements OnInit {
+  private languageId = 'ov';
   editorOptions = {
     theme: 'vs-dark',
-    language: 'ov',
+    language: this.languageId,
     minimap: {
       enabled: false
     },
@@ -27,14 +31,15 @@ export class RulesetEditorComponent implements OnInit {
     private route: ActivatedRoute,
     private rulesetsService: RulesetsService,
     private themeService: ThemeService,
-  ) { }
+  ) {
+  }
 
   ngOnInit(): void {
     this.ruleset$ = this.route.paramMap
-    .pipe(
-      map(params => params.get('id')),
-      switchMap(id => this.rulesetsService.getRuleset(id)),
-    );
+      .pipe(
+        map(params => params.get('id')),
+        switchMap(id => this.rulesetsService.getRuleset(id)),
+      );
 
     this.themeService.darkThemeActive$.subscribe((isDark) => {
       if (this.editor !== undefined) {
@@ -44,7 +49,57 @@ export class RulesetEditorComponent implements OnInit {
   }
 
   monacoInit(editor) {
-    this.editor = monaco.editor;
-    console.log('monaco initialized');
+    // install Monaco language client services
+    MonacoServices.install(editor);
+    // create the web socket
+    const url = this.createUrl();
+    const webSocket = this.createWebSocket(url);
+    // listen when the web socket is opened
+    listen({
+      webSocket,
+      onConnection: (connection: MessageConnection) => {
+        // create and start the language client
+        const languageClient = this.createLanguageClient(connection);
+        const disposable = languageClient.start();
+        connection.onClose(() => disposable.dispose());
+      }
+    });
+  }
+
+  public createUrl(): string {
+    return 'ws://localhost:3010/ovLanguage';
+  }
+
+  public createLanguageClient(connection: MessageConnection): MonacoLanguageClient {
+    return new MonacoLanguageClient({
+      name: `${this.languageId.toUpperCase()} Client`,
+      clientOptions: {
+        // use a language id as a document selector
+        documentSelector: [this.languageId],
+        // disable the default error handler
+        errorHandler: {
+          error: () => ErrorAction.Continue,
+          closed: () => CloseAction.DoNotRestart
+        }
+      },
+      // create a language client connection from the JSON RPC connection on demand
+      connectionProvider: {
+        get: (errorHandler, closeHandler) => {
+          return Promise.resolve(createConnection(connection as any, errorHandler, closeHandler));
+        }
+      }
+    });
+  }
+
+  public createWebSocket(socketUrl: string): WebSocket {
+    const socketOptions = {
+      maxReconnectionDelay: 10000,
+      minReconnectionDelay: 1000,
+      reconnectionDelayGrowFactor: 1.3,
+      connectionTimeout: 10000,
+      maxRetries: Infinity,
+      debug: false
+    };
+    return new ReconnectingWebSocket.default(socketUrl, [], socketOptions);
   }
 }
