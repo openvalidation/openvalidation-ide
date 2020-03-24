d0066188fce6cb0d924b36d814b7c5fbcc5a1075
aletutto <t.alessandro@web.de>
2020-03-24 17:54:31 +0100

    #68 Added integration test and refactored packages

--- backend/src/main/java/de/openvalidation/openvalidationidebackend/maintenance/ResetController.java
+++ backend/src/main/java/de/openvalidation/openvalidationidebackend/maintenance/ResetController.java
@@ -1,5 +1,6 @@
-package de.openvalidation.openvalidationidebackend.entities;
+package de.openvalidation.openvalidationidebackend.maintenance;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -7,8 +8,11 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.SQLException;
+
 @CrossOrigin
 @RestController
+@Hidden
 public class ResetController {
   private ResetService resetService;
 
@@ -19,7 +23,7 @@ public class ResetController {
 
   @DeleteMapping(value = "/reset")
   @ResponseStatus(HttpStatus.NO_CONTENT)
-  public void resetDatabaseToInitialState() {
+  public void resetDatabaseToInitialState() throws SQLException {
     resetService.resetDatabaseToInitialState();
   }
 }
--- backend/src/main/java/de/openvalidation/openvalidationidebackend/maintenance/ResetService.java
+++ backend/src/main/java/de/openvalidation/openvalidationidebackend/maintenance/ResetService.java
@@ -1,4 +1,4 @@
-package de.openvalidation.openvalidationidebackend.entities;
+package de.openvalidation.openvalidationidebackend.maintenance;
 
 import de.openvalidation.openvalidationidebackend.entities.ruleset.RulesetRepository;
 import de.openvalidation.openvalidationidebackend.entities.schema.SchemaRepository;
@@ -9,6 +9,9 @@ import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.stereotype.Service;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 
 @Service
 public class ResetService {
@@ -23,12 +26,20 @@ public class ResetService {
     this.dataSource = dataSource;
   }
 
-  public void resetDatabaseToInitialState() {
+  public void resetDatabaseToInitialState() throws SQLException {
     rulesetRepository.deleteAll();
     schemaRepository.deleteAll();
 
     ResourceDatabasePopulator resourceDatabasePopulator = new ResourceDatabasePopulator();
-    resourceDatabasePopulator.addScript(new ClassPathResource("data-postgresql.sql"));
-    DatabasePopulatorUtils.execute(resourceDatabasePopulator, dataSource);
+
+    try (Connection connection = dataSource.getConnection()) {
+      if (connection.getMetaData().getDatabaseProductName().equalsIgnoreCase("h2")) {
+        resourceDatabasePopulator.addScript(new ClassPathResource("data-h2.sql"));
+      } else {
+        resourceDatabasePopulator.addScript(new ClassPathResource("data-postgresql.sql"));
+      }
+
+      DatabasePopulatorUtils.execute(resourceDatabasePopulator, dataSource);
+    }
   }
 }
