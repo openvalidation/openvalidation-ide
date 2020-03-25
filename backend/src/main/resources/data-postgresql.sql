59402ecdb6806c63785ac6b3ff1c82caa5777578
Stepan Hagge <stephan_oliver.hagge@smail.th-koeln.de>
2020-03-25 12:14:59 +0100

    #89 Basic initial ruleset

--- backend/src/main/resources/data-postgresql.sql
+++ backend/src/main/resources/data-postgresql.sql
@@ -26,3 +26,63 @@ INSERT INTO ruleset (ruleset_Id, created_at, created_by, description, name, rule
 VALUES ('d0d95814-678b-11ea-bc55-0242ac130003', '2020-03-11 20:20:34.884', 'Validaria', 'Validates the name',
         'Name validation', 'your name HAS to be Validaria', '9d203228-678a-11ea-bc55-0242ac130003')
 ON CONFLICT (ruleset_Id) DO NOTHING;
+
+
+INSERT INTO schema (schema_id)
+VALUES ('c4dc0011-f794-4265-8bd5-1e0de59ae19c')
+ON CONFLICT (schema_id) DO NOTHING;
+
+INSERT INTO attribute (attribute_id, attribute_type, name, value)
+VALUES ('84d170f6-17ec-45ad-9af3-11e07666a216', 2, 'name', 'Sherlock Holmes')
+ON CONFLICT (attribute_id) DO NOTHING;
+INSERT INTO attribute (attribute_id, attribute_type, name, value)
+VALUES ('766c4b27-7ed6-4677-8afd-b1742f0643db', 1, 'age', '60')
+ON CONFLICT (attribute_id) DO NOTHING;
+INSERT INTO attribute (attribute_id, attribute_type, name, value)
+VALUES ('fa9998ed-4ac1-4cfe-939a-7b87021e2a51', 2, 'residence', 'Baker Street 221b')
+ON CONFLICT (attribute_id) DO NOTHING;
+INSERT INTO attribute (attribute_id, attribute_type, name, value)
+VALUES ('182db919-8b67-4f76-a89d-6f66f7394f1f', 0, 'signed', 'false')
+ON CONFLICT (attribute_id) DO NOTHING;
+
+INSERT INTO schema_attributes (schema_schema_id, attributes_attribute_id)
+VALUES ('c4dc0011-f794-4265-8bd5-1e0de59ae19c',
+        '84d170f6-17ec-45ad-9af3-11e07666a216')
+ON CONFLICT (schema_schema_id, attributes_attribute_id) DO NOTHING;
+INSERT INTO schema_attributes (schema_schema_id, attributes_attribute_id)
+VALUES ('c4dc0011-f794-4265-8bd5-1e0de59ae19c',
+        '766c4b27-7ed6-4677-8afd-b1742f0643db')
+ON CONFLICT (schema_schema_id, attributes_attribute_id) DO NOTHING;
+INSERT INTO schema_attributes (schema_schema_id, attributes_attribute_id)
+VALUES ('c4dc0011-f794-4265-8bd5-1e0de59ae19c',
+        'fa9998ed-4ac1-4cfe-939a-7b87021e2a51')
+ON CONFLICT (schema_schema_id, attributes_attribute_id) DO NOTHING;
+INSERT INTO schema_attributes (schema_schema_id, attributes_attribute_id)
+VALUES ('c4dc0011-f794-4265-8bd5-1e0de59ae19c',
+        '182db919-8b67-4f76-a89d-6f66f7394f1f')
+ON CONFLICT (schema_schema_id, attributes_attribute_id) DO NOTHING;
+
+INSERT INTO ruleset (ruleset_Id, created_at, created_by, description, name, rules, schema_schema_id)
+VALUES ('5f19d628-2e90-4830-85ea-07cf29d3ebc5', now(), 'OpenVALIDATION-IDE Team',
+        'Examples for basic rules',
+        'Basic ruleset',
+        'IF the age of the applicant is LESS than 18\n' ||
+        'THEN you must be at least 18 years old\n\n' ||
+        'the contract MUST be signed\n\n' ||
+        'IF  the age of the applicant is GREATER 18\n' ||
+        '  AND  the name of the applicant IS Mycroft Holmes.\n' ||
+        '  OR his name IS Sherlock Holmes.\n' ||
+        'THEN  the applicant is a genius\n\n\n' ||
+        '  the age of the applicant is LESS than 18 years old\n' ||
+        'AS underage\n\n' ||
+        '  Age - 18\n' ||
+        'AS professional experience\n\n' ||
+        'IF the applicant is underage\n' ||
+        '  AND his residence IS NOT Baker Street 221b\n' ||
+        'THEN You must be at least 18 years old and come from Baker Street 221b.\n' ||
+        'IF the applicant is underage\n' ||
+        '  OR his professional experience is LESS than 5 years\n' ||
+        'THEN You must be at least 18 years old and have work experience\n' ||
+        '  of at least 5 years\n',
+        'c4dc0011-f794-4265-8bd5-1e0de59ae19c')
+ON CONFLICT (ruleset_Id) DO NOTHING;
