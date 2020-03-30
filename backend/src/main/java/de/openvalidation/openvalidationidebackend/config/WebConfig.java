ea11236ded0fcb7a13be51da9bfe13dabf3a2e01
aletutto <t.alessandro@web.de>
2020-03-30 15:32:29 +0200

    #115 Allow HTTP POST and DELETE in CORS header policy

--- backend/src/main/java/de/openvalidation/openvalidationidebackend/config/WebConfig.java
+++ backend/src/main/java/de/openvalidation/openvalidationidebackend/config/WebConfig.java
@@ -2,6 +2,7 @@ package de.openvalidation.openvalidationidebackend.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -16,6 +17,7 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
+        .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(), HttpMethod.DELETE.name())
         .allowedOrigins(corsHeaders.trim().concat(",http://localhost").split("\\s*,\\s*"));
   }
 }
