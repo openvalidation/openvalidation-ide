cb10e543b62807bd5891999e5057f2393da29b0c
xLacrima <44511324+xLacrima@users.noreply.github.com>
2020-03-31 16:19:27 +0200

    animate ruleset creator

--- frontend/src/app/ruleset-creator/ruleset-creator.component.ts
+++ frontend/src/app/ruleset-creator/ruleset-creator.component.ts
@@ -1,17 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { RulesetCreateDto, RulesetsBackendService } from '@ovide/backend';
 
 import * as faker from 'faker';
 import { Router } from '@angular/router';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 
 @Component({
   selector: 'ovide-ruleset-creator',
   templateUrl: './ruleset-creator.component.html',
-  styleUrls: ['./ruleset-creator.component.scss']
+  styleUrls: ['./ruleset-creator.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ transform: 'scale(0.9)', opacity: 0.2 }),
+        animate('.3s ease-out')
+      ])
+    ])
+  ]
 })
 export class RulesetCreatorComponent implements OnInit {
+
+
+  @HostBinding('[@slideInOut]')
 
   form: FormGroup;
 
