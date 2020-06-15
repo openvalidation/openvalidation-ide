bfd447826126a4974a89a3c284197d22016fc248
Johannes Quack <33766870+Jaykju@users.noreply.github.com>
2020-06-15 23:36:17 +0200

    Save current theme in cookie (#176)
    * #173 Check highlighting if any word contains a keyword
    Fix an error where the object that was being iterated was modified at the same time causing reference issues.
    
    * #175 Save current theme in a cookie

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -2,7 +2,7 @@ import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { OvideBackendApiModule, BASE_PATH } from './backend';
 import { environment } from 'environments/environment';
-
+import { CookieService } from 'ngx-cookie-service';
 
 
 @NgModule({
@@ -11,7 +11,8 @@ import { environment } from 'environments/environment';
     OvideBackendApiModule
   ],
   providers: [
-    {provide: BASE_PATH, useValue: environment.API_BASE_PATH}
+    {provide: BASE_PATH, useValue: environment.API_BASE_PATH},
+    CookieService
   ]
 })
 export class CoreModule {
